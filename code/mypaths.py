6300b697dbf9d05b9fe13fba587a6802b5b0477b
Dennis Sergeev <dennis.sergeev@gmail.com>
2018-06-09 13:47:58 +0100

    add stars tracks, update mypaths

--- code/mypaths.py
+++ code/mypaths.py
@@ -9,10 +9,15 @@ from pathlib import Path
 # Top-level directory containing code and data (one level up)
 topdir = Path('.').absolute().parent
 
+# Local data
+accdir = topdir/'data'/'tracks'/'accacia'
+acctracks = accdir/'pmc_loc_time_ch4_20Mar-02Apr.txt'
+starsdir = topdir/'data'/'tracks'/'stars'
+starstracks = starsdir/'PolarLow_tracks_North_2002_2011'
+
 # Temporary
 phddir = Path.home()/'phd'
 plotdir = phddir/'plots'/'climatology'
-starsdir = phddir/'STARSDAT'
 trackresdir = phddir/'pmc_tracking'/'results'
 procdir = phddir/'pmc_tracking'/'results'/'processed_data'
 runsgridfile = trackresdir/'runs_grid.json'
