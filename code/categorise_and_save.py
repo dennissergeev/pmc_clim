edcad30d112a1be48672be4f750caa3525c180bd
Dennis Sergeev <dennis.sergeev@gmail.com>
2018-06-10 10:55:23 +0100

    add a script to categorise selected runs; new dependency: pyarrow

---
+++ code/categorise_and_save.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Categorise selected runs and save the full dataframe for later use
+"""
+import numpy as np
+from tqdm import tqdm
+import xarray as xr
+
+import mypaths
+from common_defs import cat_kw, winters
+
+from octant.core import TrackRun
+
+# Select runs
+runs2process = dict(era5=[0], interim=[100, 106])
+
+# Load land-sea mask
+lsm = xr.open_dataarray(mypaths.era5_dir / 'lsm.nc').squeeze()
+lon2d, lat2d = np.meshgrid(lsm.longitude, lsm.latitude)
+
+
+period = f'{winters[0][:4]}_{winters[-1][-4:]}'
+
+for dataset, run_nums in tqdm(runs2process.items(), desc='dataset'):
+    for run_num in tqdm(run_nums, leave=False, desc='run_num'):
+        TR = TrackRun()
+        for winter in tqdm(winters, desc='winter', leave=False):
+            track_res_dir = (mypaths.trackresdir / dataset
+                             / f'run{run_num:03d}' / winter)
+            TR += TrackRun(track_res_dir)
+
+        TR.categorise(lsm=lsm, **cat_kw)
+
+        TR.data.to_parquet((mypaths.trackresdir /
+                            f'{dataset}_run{run_num:03d}_{period}.parquet'),
+                           engine='pyarrow')
